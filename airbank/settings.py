4876a053a363069ebc42e78586fcdd2bff6a6701
DADEVFA <davidfarah193@gmail.com>
2021-07-30 23:58:57 +0000

    Removed Secret Key

--- airbank/settings.py
+++ airbank/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "232dz(dxtd+4bn#ll*h=9f#5@kf*j!^4d=i-t00)st-zbf5a#7")
+SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
